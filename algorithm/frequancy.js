fae12284f584476713a2e6cc18fae7299c1ad90c
sin-yusu@sinyusucBookPro <tlsdbtn0507@naver.com>
2023-10-19 23:26:27 +0900

    [algo] multi pointer

--- algorithm/frequancy.js
+++ algorithm/frequancy.js
@@ -10,16 +10,6 @@ function same(first, sec) {
   return true;
 }
 
-// console.log(same([1, 2, 3], [1, 4, 9]));
-// console.log(same([1, 2, 3], [9, 4, 1]));
-// console.log(same([1, 2, 3], [1, 9]));
-// console.log(same([1, 2, 1], [4, 4, 1]));
-// console.log(same([1, 2, 1, 2, 5], [25, 4, 4, 1, 1]));
-// same([1, 2, 3], [1, 4, 9]);
-// same([1, 2, 3], [9, 4, 1]);
-// same([1, 2, 3], [1, 9]);
-// same([1, 2, 1], [4, 4, 1]);
-
 function validAnagram(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
 
