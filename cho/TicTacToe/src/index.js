9e94dc7a8a650a8764c10aa70b750b4cceccf161
yusu <tlsdbtn0507@naver.com>
2023-10-26 23:47:52 +0900

    [cho] click issue done

--- cho/TicTacToe/src/index.js
+++ cho/TicTacToe/src/index.js
@@ -2,47 +2,22 @@ import { data } from "./boxes.js";
 import { click } from "./click.js";
 import { checkOver } from "./over.js";
 
-const table = document.createElement("table");
-const tbody = document.createElement("tbody");
+const tbody = document.querySelector(".table tbody");
 
 const tableMaker = () => {
   //table 생성
   const cells = data();
-  // console.log(data());
   cells.forEach((e) => {
     const tr = document.createElement("tr");
     //칸 생성
     e.forEach((i) => {
       const td = document.createElement("td");
       td.classList = `${cells.indexOf(e)}-${e.indexOf(i)}`;
-      // td.textContent = "a";
-      td.addEventListener("click", (event) => {
-        console.log(event);
-        // click(event);
-      });
       tr.append(td);
     });
     tbody.append(tr);
+    tbody.addEventListener('click',(event)=>{click(event)})
   });
-  table.appendChild(tbody);
 
-  document.body.appendChild(table);
 };
 tableMaker();
-// // table.addEventListener("click", click);
-// document.querySelectorAll("td").forEach((el) => {
-//   el.addEventListener("click", (e) => {
-//     console.log(e);
-//   });
-//   // e.onclick = function (e) {
-//   console.log(el);
-//   // };
-// });
-
-// const tds = document.querySelectorAll("td");
-// tds.forEach((e) => {
-//   console.log(e);
-//   e.addEventListener("click", (ev) => {
-//     console.log(ev.target);
-//   });
-// });
