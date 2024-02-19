23cf6c8ec4c272a9d14f3864e113b1f38aa0f584
yusuShin <tlsdbtn0507@naver.com>
2024-02-19 22:10:54 +0900

    [Algo] basic folder

---
+++ algorithm/basic/day2.js
@@ -0,0 +1,8 @@
+function solution(my_string, overwrite_string, s) {
+    
+    const str = [...my_string];
+    
+    str.splice(s,overwrite_string.length,overwrite_string);
+
+    return str.join('')
+}
