24c44e117fe050d2ed380b16cbab40215dc4ec95
yusuShin <tlsdbtn0507@naver.com>
2024-02-21 23:32:15 +0900

    [Algo] day4

---
+++ algorithm/basic/day4.js
@@ -0,0 +1,26 @@
+function solution(code) {
+    let ret = [];
+    let mode = 0;    
+    for(let i = 0; i < code.length; i++){
+        if(mode === 0){
+            if(code[i] !== '1' && i%2 === 0) {
+                ret.push(code[i])
+            }
+            if(code[i] === '1') {
+                mode = 1
+            }
+        } else {
+            if(code[i] !== '1' && i%2 === 1) {
+                ret.push(code[i])
+            }
+            if(code[i] === '1') {
+                mode = 0
+            }
+        }
+        
+        
+    }
+    const res = ret.join('')
+
+    return res === '' ? 'EMPTY' : res;
+}
