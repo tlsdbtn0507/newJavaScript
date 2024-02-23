30e72b3498e8319b296883e825627717461f8e90
yusuShin <tlsdbtn0507@naver.com>
2024-02-23 16:14:02 +0900

    [docker] 아주 귀찮은 -v 왜냐하면 stop을 계속 해야하기 때문

--- docker-node-start/server.js
+++ docker-node-start/server.js
@@ -3,7 +3,7 @@ const PORT = 8080;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("반갑습니다.");
+  res.send("Bothering Refresh.");
 })
 app.listen(PORT, () => {
   console.log('애플리케이션이 실행됐습니다');
