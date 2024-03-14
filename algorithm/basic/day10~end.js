f691802ca2a5ccdccae5c1255381a087c501069a
yusuShin <tlsdbtn0507@naver.com>
2024-03-14 23:51:55 +0900

    [algo] day14-1

--- algorithm/basic/day10~end.js
+++ algorithm/basic/day10~end.js
@@ -105,3 +105,11 @@ function solution13_left_right(str_list) {
     return emptyCond ? [] : answer;
 
 }
+//14
+function solution14_1(num_list) {
+    
+    const o = num_list.filter((_,i)=> i%2 !== 0).reduce((a,b)=>a+b)
+    const a = num_list.filter((_,i)=> i%2 === 0).reduce((a,b)=>a+b)
+    
+    return o > a ? o : a;
+}
