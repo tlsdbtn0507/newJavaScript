70e1ce9f6abb45dc1a7abf72c7194737e18e4843
yusuShin <tlsdbtn0507@naver.com>
2024-03-19 22:37:17 +0900

    [algo]day17

--- algorithm/basic/day15~.js
+++ algorithm/basic/day15~.js
@@ -4,3 +4,31 @@ function solution17_1(myString, pat) {
     
     return myString.slice(0,ind+pat.length);
 }
+
+//17_2
+function solution17_2(myString, pat) {
+    let answer = 0;
+    
+    for(let i = 0; i < myString.length; i++){
+      if (myString
+        .slice(i, i + pat.length)
+        .includes(pat)) answer++;
+    }
+    
+    return answer;
+}
+
+//17_3
+function solution17_3(strArr) {
+    return strArr.filter(e=>!e.includes('ad'));
+}
+
+//17_4
+function solution17_4(my_string) {
+    return my_string.split(' ');
+}
+
+//17_5
+function solution17_5(my_string) {
+    return my_string.trim().split(' ').filter(e=>e!=='')
+}
