28f86ffe94e22b74bc80f37dc7998a727b4934df
yusuShin <tlsdbtn0507@naver.com>
2024-03-20 17:36:16 +0900

    [algo]day18

--- algorithm/basic/day15~.js
+++ algorithm/basic/day15~.js
@@ -32,3 +32,40 @@ function solution17_4(my_string) {
 function solution17_5(my_string) {
     return my_string.trim().split(' ').filter(e=>e!=='')
 }
+
+//18_1
+function solution18_1(myString) {
+
+    return myString.split('x').map(e=>e.length);
+}
+
+//18_2
+function solution18_2(myString) {
+    return myString.split('x').filter(e=>e!=='').sort();
+}
+
+//18_3
+function solution18_3(binomial) {
+    
+    const [a,op,b] = binomial.split(' ')
+    
+    if(op === '*') return +a*+b;
+    if(op === '+') return +a + +b;
+    if(op === '-') return +a - +b
+    
+}
+
+//18_4
+function solution(myString, pat) {
+    var answer = 0;
+  return [...myString]
+    .map(e => e === "A" ? "B" : 'A')
+    .join('')
+    .includes(pat) ? 1 : 0;
+}
+
+//18_5
+function solution18_5(rny_string) {
+    var answer = '';
+    return rny_string.replaceAll('m','rn');
+}
