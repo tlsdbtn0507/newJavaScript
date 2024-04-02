a9031c75cf2f77a44b4a6b73dad9d88a856d0c5b
yusuShin <tlsdbtn0507@naver.com>
2024-04-02 22:04:04 +0900

    [algo] hash <clothes>

--- algorithm/highScoreKit/hash.js
+++ algorithm/highScoreKit/hash.js
@@ -10,3 +10,20 @@ function ponkenmon(nums) {
 
     return arr.length > half ? half : arr.length;
 }
+
+function connieClothes(clothes) {
+    let arr = [];
+
+    for(let i = 0; i < clothes.length; i++){
+        if (!arr.flat().includes(clothes[i][1]))
+            arr.push([clothes[i][1],[clothes[i][0]]]);
+        else {
+            let j = arr.findIndex(e => e[0] === clothes[i][1]);
+            arr[j][1].push(clothes[i][0]);
+        }
+            
+    }
+    
+    const count = arr.map(e => e[1].length + 1).reduce((a, b) => a * b) - 1;
+    return count;
+}
