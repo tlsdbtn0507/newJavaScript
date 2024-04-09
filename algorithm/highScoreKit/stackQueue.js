1a64fd545d40f82baffb3649ab2d97e041c004d4
yusuShin <tlsdbtn0507@naver.com>
2024-04-09 23:49:39 +0900

    [algo] solving programming

---
+++ algorithm/highScoreKit/stackQueue.js
@@ -0,0 +1,28 @@
+function programming(progresses, speeds) {
+    const answer = [];
+    
+    let days= []
+    for(let i = 0; i < progresses.length; i++){
+        let d = 1
+        for(let j = +progresses[i]; j+speeds[i] < 100; j+=speeds[i]){
+            d++
+        }
+        days.push(d)
+    }
+    let c = 1
+    for(let i = 1; i <= days.length; i++) {
+        
+        console.log(days,c,i)
+        if(days[0] < days[i]) {
+            days.shift()
+            answer.push(c);
+            console.log(c)
+        }
+        if(days[0] > days[i]) {
+            c++
+        }
+        
+    }
+    
+    return answer;
+}
