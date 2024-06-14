76ef0f88d741b2f6c34b342adca0d6e732ca41ec
yusuShin <tlsdbtn0507@naver.com>
2024-06-14 23:14:51 +0900

    [구현] 쥐 사이즈 / 최대치

--- algorithm/simulation/rat.js
+++ algorithm/simulation/rat.js
@@ -3,18 +3,36 @@ const readline = require('readline');
 
 (async () => {
 	let rl = readline.createInterface({ input: process.stdin });
+	let arr = [];
 	let total
 	for await (const line of rl) {
 		const l = line.split(' ').map(e=>+e);
 		
-		if(l.length  === 1) {
-			total = l[0];
-			continue;
-		}
+		if(l.length  === 1){ 
+			total = l[0]
+			continue
+		};
 		
-		console.log(total,l);
+		// arr.push(l.sort((a,b)=>a-b,0))
+		arr.push(l)
 		rl.close();
 	}
+	
+	
+	
+	// let mostVals = [];
+	const [clean,dirty] = arr
+	// console.log(clean,dirty)
+
+	const getMostVal = (arr)=>{
+		const result = []
+		for(let i = 0; i< arr.length; i++){
+			result.push([arr.filter(e=> (arr[i] >= e-2 && arr[i] <= e+2 ) && e).length,i])
+		}
+		return result.sort((a,b)=>a[0]-b[0],0)
+		// return result
+	}
+	console.log(getMostVal(clean))
 	
 	process.exit();
 })();
