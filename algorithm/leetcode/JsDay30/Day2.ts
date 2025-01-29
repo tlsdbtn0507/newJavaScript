a7df02d481bcc87028addb34b20e91753a7fcde3
yusuShin <tlsdbtn0507@naver.com>
2025-01-29 21:16:56 +0900

    day2 filter

--- algorithm/leetcode/JsDay30/Day2.ts
+++ algorithm/leetcode/JsDay30/Day2.ts
@@ -8,3 +8,17 @@ function map(arr: number[], fn: (n: number, i: number) => number): number[] {
     
     return result; // 변환된 배열 반환
 };
+
+type Fn = (n: number, i: number) => any
+
+function filter(arr: number[], fn: Fn): number[] {
+    const result:number[] = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if(fn(arr[i],i)) {
+            result.push(arr[i])
+        }
+    }
+
+    return result
+};
