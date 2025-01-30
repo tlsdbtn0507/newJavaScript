eed51e3b8c65b6afa75dfd51efad7d973743b4be
yusuShin <tlsdbtn0507@naver.com>
2025-01-30 23:44:26 +0900

    day2 파일명 변경

---
+++ algorithm/leetcode/JsDay30/Day3_FunctionTransition.ts
@@ -0,0 +1,11 @@
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    return function(x) {
+        let result = x;
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+        return result;
+    };
+};
